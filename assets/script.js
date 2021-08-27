1f0fc4413e830dbac376906c07b0563d980e0144
Ben Townsend <RBT000011@gmail.com>
2021-08-26 21:29:48 -0500

    added to JS, got date to display in console.log

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+let time = document.querySelector(".time");
+const event = document.querySelector(".event");
+const save = document.querySelector(".save");
+const dateDisp = document.querySelector(".dateDisplay");
+
+const date = new Date();
+console.log(date)

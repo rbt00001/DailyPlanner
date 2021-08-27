dfdeef06fd40039655e70aa279cd6b53cbd2f13c
Ben Townsend <RBT000011@gmail.com>
2021-08-27 17:35:16 -0500

    got date to appear at load of page

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,9 @@
 let time = document.querySelector(".time");
-const event = document.querySelector(".event");
+const events = document.querySelector(".events");
 const save = document.querySelector(".save");
 const dateDis = document.querySelector(".dateDisplay");
 
 const date = new Date();
 console.log(date)
 
-dateDis
+document.getElementById("dateDisplay").innerHTML = date.toLocaleDateString();

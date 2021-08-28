9a80b795f36c00fc1a2c64110185f5a0dc8b19a9
Ben Townsend <RBT000011@gmail.com>
2021-08-28 09:25:06 -0500

    Added Jquery to print to local storage for day planner

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,22 @@
+console.log("hello")
 let time = document.querySelector(".time");
-const events = document.querySelector(".events");
-const save = document.querySelector(".save");
+const events = document.querySelector(".eventContent");
+// let saveBtn = document.getElementById("7")
 const dateDis = document.querySelector(".dateDisplay");
 
 const date = new Date();
 console.log(date)
 
 document.getElementById("dateDisplay").innerHTML = date.toLocaleDateString();
+
+$("#7").on("click", (e) => {
+   // $("#7").prev().child();
+    console.log( $("#7input").val())
+    localStorage.setItem("7", $("#7input").val() )
+})
+
+/*
+saveBtn.addEventListener("click", (e) => {
+    console.log("I work")
+
+}) */
